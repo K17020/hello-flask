fb2cf0452223b49585b3e00ba8365a882a3db863
Kevin Tam <tam.kevin@hotmail.com>
2017-06-18 12:27:46 -0700

    finalze the time validation

--- main.py
+++ main.py
@@ -2,55 +2,55 @@ from flask import Flask, request
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
- 
+
 form = """
 <!doctype html>
 <html>
     <body>
-    <!--This sends the text from the input to the hello fuction below-->    
         <form action="/hello" method="post">
-            <label for="first-name">First Name</label>
-            <!--This creates the text field for you to input the text-->    
+            <label for="first-name">First Name:</label>
             <input id="first-name" type="text" name="first_name" />
-            <!--This creates the button to submit text-->   
-            <input type="submit"/>
+            <input type="submit" />
         </form>
     </body>
 </html>
-
 """
 
 @app.route("/")
 def index():
     return form
 
-@app.route("/hello",methods=['POST'])
+@app.route("/hello", methods=['POST'])
 def hello():
     first_name = request.form['first_name']
-    return '<h1>Hello, '+ first_name + '</h1>'
+    return '<h1>Hello, ' + first_name + '</h1>'
+
 
 time_form = """
     <style>
         .error {{ color: red; }}
-    </style
+    </style>
     <h1>Validate Time</h1>
     <form method='POST'>
         <label>Hours (24-hour format)
-            <input type='text' name='hours' value='{hours}'/>
+            <input name="hours" type="text" value='{hours}' />
         </label>
         <p class="error">{hours_error}</p>
         <label>Minutes
-            <input type='text'name='minutes' value='{minutes}'/>
+            <input name="minutes" type="text" value='{minutes}' />
         </label>
         <p class="error">{minutes_error}</p>
-        <input type="submit" value="validate"/>
+        <input type="submit" value="Validate" />
     </form>
-"""
+    """
+
 @app.route('/validate-time')
 def display_time_form():
-    return time_form.format(hours='',hours_error='', minutes='',minutes_error='')
+    return time_form.format(hours='', hours_error='',
+        minutes='', minutes_error='')
 
-def is_initger(num):
+
+def is_integer(num):
     try:
         int(num)
         return True
@@ -63,29 +63,34 @@ def validate_time():
     hours = request.form['hours']
     minutes = request.form['minutes']
 
+    hours_error = ''
+    minutes_error = ''
+
     if not is_integer(hours):
         hours_error = 'Not a valid integer'
-        hours = ""
+        hours = ''
     else:
         hours = int(hours)
         if hours > 23 or hours < 0:
-            hours_error = 'hours value out of range (0-23)'
-            hours = ""
+            hours_error = 'Hour value out of range (0-23)'
+            hours = ''
 
     if not is_integer(minutes):
         minutes_error = 'Not a valid integer'
-        minutes = ""
+        minutes = ''
     else:
         minutes = int(minutes)
-           if minutes > 59 or minutes < 0:
-            minutes_error = 'minutes value out of range (0-59)'
-            minutes = ""
+        if minutes > 59 or minutes < 0:
+            minutes_error = 'Minutes value out of range (0-59)'
+            minutes = ''
 
     if not minutes_error and not hours_error:
-        # success messsage
         return "Success!"
     else:
-        return  time_form.format(hours_error=hours_error,
-        minutes_error=minutes_error,hours=hours,minutes=minutes)
+        return time_form.format(hours_error=hours_error,
+            minutes_error=minutes_error,
+            hours=hours,
+            minutes=minutes)
+
 
 app.run()
